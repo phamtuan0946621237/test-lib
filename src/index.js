256c6184d441b30adb1d025026cef1261a874bce
Pham Tuan <phamtuannd200997@gmail.com>
2022-06-01 14:20:28 +0700

    #update

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// export function multiply(a: number, b: number): Promise<number> {
+//   return Promise.resolve(a * b);
+// }
+import React from 'react'
+import {Text} from 'react-native'
+export const __ = () => {
+  return <Text>jejeje</Text>
+}
